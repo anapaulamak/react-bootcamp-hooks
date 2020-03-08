6b5ee81f61c368652697fe5f84eeeaa3ef7f6aff
ana paula <anapaulamak@gmail.com>
2020-03-08 10:21:06 -0300

    Add detail component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './components/header/header'
 import CardList from './components/cardList/cardList'
+import Detail from './components/detail/detail'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CardList />
+      {/* <CardList /> */}
+      <Detail />
     </div>
   )
 }
--- src/components/detail/detail.js
+++ src/components/detail/detail.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import axios from "axios";
 
-export default function CardList (props){
+export default function Detail (props){
+  const [item, setItem] = useState([])
 
+  useEffect(() => {
+    let movie_id = '2baf70d1-42bb-4437-b551-e5fed5a87abe';
+
+    axios
+      .get(`https://ghibliapi.herokuapp.com/films/${movie_id}`)
+      .then(({ data }) => {
+        setItem(data);
+      })
+      .catch(function(err) {
+        console.error(err);
+      })
+  },[]);
 
   return(
     <>
       <div className="detail">
         <label> Título: </label>
-        <p>{props.title}</p>
+        <p>{item.title}</p>
 
         <label> Descrição: </label>
-        <p>{props.description}</p>
+        <p>{item.description}</p>
 
         <label> Diretor: </label>
-        <p>{props.director}</p>
+        <p>{item.director}</p>
 
         <label> Produtor: </label>
-        <p>{props.producer}</p>
+        <p>{item.producer}</p>
 
         <label> Data: </label>
-        <p>{props.release_date}</p>
+        <p>{item.release_date}</p>
 
         <label> Score: </label>
-        <p>{props.rt_score}</p>
+        <p>{item.rt_score}</p>
 
       </div>
     </>
